d5623e3d114574098d7a2a9f7c1c45791ae9073b
it-jcg <joyce.garnett@magna.com>
2022-07-13 11:40:36 -0400

    added the rest of the tests for Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,27 @@ test('create Employee object', () => {
     expect(empoylee.id).toBe(1);
     expect(empoylee.email).toBe('john.doe@test.com');
 });
+
+test('get employee name', () => {
+    const empoylee = new Employee('John Doe', 1, 'john.doe@test.com');
+    
+    expect(empoylee.getName()).toBe('John Doe');
+});
+
+test('get employee id', () => {
+    const empoylee = new Employee('John Doe', 1, 'john.doe@test.com');
+
+    expect(empoylee.getId()).toBe(1);
+});
+
+test('get employee email', () => {
+    const employee = new Employee('John Doe', 1, 'john.doe@test.com');
+
+    expect(employee.getEmail()).toBe('john.doe@test.com');
+});
+
+test('get employee', () => {
+    const empoylee = new Employee('John Doe', 1, 'john.doe@test.com');
+
+    expect(employee.getRole()).toBe('Employee');
+})
